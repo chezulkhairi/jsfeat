114fe2a95037f4cf71beb5969954d1f3170b226e
CheZulkhairi <chezulkhairi.abdullah@gmail.com>
2018-11-21 11:42:41 +0000

    Update index3.js

--- delaunay/js/index3.js
+++ delaunay/js/index3.js
@@ -356,6 +356,40 @@ if(img.width) {
     img.onload = init;
 }
 
+//--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+var width  = window.innerWidth / 2,
+        height = window.innerHeight;
+    var scene = new THREE.Scene();
+    var axes = new THREE.AxisHelper(100);
+    scene.add(axes);
+    var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+    camera.position.set(20, -80, 100);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    var geometry = new THREE.PlaneGeometry(60, 60, 9, 9);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x333333, 
+        wireframe: true
+    });
+    var plane = new THREE.Mesh(geometry, material);
+    scene.add(plane);
+    var controls = new THREE.TrackballControls(camera); 
+    document.getElementById('webgl1').appendChild(renderer.domElement);
+    render();
+
+
+function render() {
+        controls.update();    
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    }
+
+
+//--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//--->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 function useCamera(isEnable) {
     if(isEnable) {
         if(!video) {
