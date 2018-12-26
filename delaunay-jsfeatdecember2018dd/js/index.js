fd2cef9fc089f34bedfaa42a43838a8d811a3210
CheZulkhairi <chezulkhairi.abdullah@gmail.com>
2018-12-26 20:16:36 +0000

    Update index.js

--- delaunay-jsfeatdecember2018dd/js/index.js
+++ delaunay-jsfeatdecember2018dd/js/index.js
@@ -189,10 +189,6 @@ function updateSource() {
         }
 
         var imageData;
-      var curr_img_pyr, prev_img_pyr, point_count, point_status, prev_xy, curr_xy;
-      max_iterations = 30;
-      epsilon = 0.01;
-      min_eigen = 0.001;
         if(config.useJSFeat) {
             imgCtx.drawImage(img, 0, 0, width, height);
             imageData = imgCtx.getImageData(0, 0, width, height);
@@ -203,17 +199,6 @@ function updateSource() {
                 var i = width*height;
                 while(--i >= 0) {
                     corners[i] = new jsfeat.keypoint_t(0,0,0,0);
-                  
-                  
-                  curr_img_pyr = new jsfeat.pyramid_t(3);
-                prev_img_pyr = new jsfeat.pyramid_t(3);
-                curr_img_pyr.allocate(640, 480, jsfeat.U8_t|jsfeat.C1_t);
-                prev_img_pyr.allocate(640, 480, jsfeat.U8_t|jsfeat.C1_t);
-
-                point_count = 0;
-                point_status = new Uint8Array(100);
-                prev_xy = new Float32Array(100*2);
-                curr_xy = new Float32Array(100*2);
                 }
               
               
@@ -514,99 +499,3 @@ function render_corners(corners, count, img, step) {
                     img[off+step] = pix;
                 }
             }
-
-
-
-
-
-
-
-
-function tick() {
-                compatibility.requestAnimationFrame(tick);
-                stat.new_frame();
-                if (video.readyState === video.HAVE_ENOUGH_DATA) {
-                    ctx.drawImage(video, 0, 0, 640, 480);
-                    var imageData = ctx.getImageData(0, 0, 640, 480);
-
-                    // swap flow data
-                    var _pt_xy = prev_xy;
-                    prev_xy = curr_xy;
-                    curr_xy = _pt_xy;
-                    var _pyr = prev_img_pyr;
-                    prev_img_pyr = curr_img_pyr;
-                    curr_img_pyr = _pyr;
-
-                    stat.start("grayscale");
-                    jsfeat.imgproc.grayscale(imageData.data, 640, 480, curr_img_pyr.data[0]);
-                    stat.stop("grayscale");
-
-                    stat.start("build image pyramid");
-                    curr_img_pyr.build(curr_img_pyr.data[0], true);
-                    stat.stop("build image pyramid");
-
-                    stat.start("optical flow lk");
-                    jsfeat.optical_flow_lk.track(prev_img_pyr, curr_img_pyr, prev_xy, curr_xy, point_count, options.win_size|0, options.max_iterations|0, point_status, options.epsilon, options.min_eigen);
-                    stat.stop("optical flow lk");
-
-                    prune_oflow_points(ctx);
-
-                    $('#log').html(stat.log() + '<br/>click to add tracking points: ' + point_count);
-                }
-            }
-
-            function on_canvas_click(e) {
-                var coords = canvas.relMouseCoords(e);
-                if(coords.x > 0 & coords.y > 0 & coords.x < canvasWidth & coords.y < canvasHeight) {
-                    curr_xy[point_count<<1] = coords.x;
-                    curr_xy[(point_count<<1)+1] = coords.y;
-                    point_count++;
-                }
-            }
-            canvas.addEventListener('click', on_canvas_click, false);
-
-            function draw_circle(ctx, x, y) {
-                ctx.beginPath();
-                ctx.arc(x, y, 4, 0, Math.PI*2, true);
-                ctx.closePath();
-                ctx.fill();
-            }
-
-            function prune_oflow_points(ctx) {
-                var n = point_count;
-                var i=0,j=0;
-
-                for(; i < n; ++i) {
-                    if(point_status[i] == 1) {
-                        if(j < i) {
-                            curr_xy[j<<1] = curr_xy[i<<1];
-                            curr_xy[(j<<1)+1] = curr_xy[(i<<1)+1];
-                        }
-                        draw_circle(ctx, curr_xy[j<<1], curr_xy[(j<<1)+1]);
-                        ++j;
-                    }
-                }
-                point_count = j;
-            }
-
-            function relMouseCoords(event) {
-                var totalOffsetX=0,totalOffsetY=0,canvasX=0,canvasY=0;
-                var currentElement = this;
-
-                do {
-                    totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-                    totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-                } while(currentElement = currentElement.offsetParent)
-
-                canvasX = event.pageX - totalOffsetX;
-                canvasY = event.pageY - totalOffsetY;
-
-                return {x:canvasX, y:canvasY}
-            }
-            HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
-
-            $(window).unload(function() {
-                video.pause();
-                video.src=null;
-            });
-        });
